9252f39117700633b538cf97a6d04db5389e9652
Priyanka Kumari <166229841+prowessclust@users.noreply.github.com>
2025-09-07 15:59:02 +0530

    Message count activated

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.')/'.env'
@@ -16,16 +16,33 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 BOT_ID = client.api_call("auth.test")['user_id']
 
+message_counts = {}
+
 @slack_event_adapter.on('message')
 def message(payload):
+  print(payload)
   event = payload.get('event', {})
   channel_id = event.get('channel')
   user_id = event.get('user')
   text = event.get('text')
 
   if BOT_ID != user_id:
+    if user_id in message_counts:
+      message_counts[user_id] += 1
+    else:
+      message_counts[user_id] = 1
     client.chat_postMessage(channel=channel_id, text=text)
     
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+  data = request.form
+  print(data)
+  user_id = data.get('user_id')
+  channel_id = data.get('channel_id')
+  message_count = message_counts.get(user_id, 0)
+  client.chat_postMessage(channel=channel_id, text=f"Message: {message_count}")
+  return Response(), 200
 
 if __name__ == "__main__":
   app.run(debug=True)
